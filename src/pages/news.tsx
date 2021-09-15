36e624fc1534a5b8d6c9f48c0762690e02014bcb
Rishabh Bhardwaj <rishabhrbs96@gmail.com>
2021-09-15 12:53:17 -0500

    fixed build errors

--- src/pages/news.tsx
+++ src/pages/news.tsx
@@ -8,19 +8,20 @@ import { Navbar } from '../components/navbar';
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-export const getServerSideProps: GetServerSideProps = async (content) => {
+export const getServerSideProps: GetServerSideProps = async () => {
   const crunchurollNewsUrl = "https://www.crunchyroll.com/en-gb/news";
   const crunchyrollUrl = "https://www.crunchyroll.com";
   const { data } = await axios.get(crunchurollNewsUrl);
 
   const $ = cheerio.load(data);
-  // console.log("calling scraper2");
+  console.log("calling scraper2");
   const listItems = $(".news-left ul .news-item");
 
-  const newsTitles = [];
+  const newsTitles:Array<Object> = [];
 
-  listItems.each((idx, el) => {
-    const newsTitle = { h2: "", h3: "", author: "", newsUrl: "" };
+  listItems.each((itx:any, el:any) => {
+    const newsTitle = { key:"", h2: "", h3: "", author: "", newsUrl: "" };
+    newsTitle.key = itx;
     newsTitle.h2 = $(el).children("h2").children("a").text();
     newsTitle.newsUrl = crunchyrollUrl + $(el).children("h2").children("a").attr('href');
     newsTitle.h3 = $(el).children("h3").children("a").text();
@@ -32,12 +33,13 @@ export const getServerSideProps: GetServerSideProps = async (content) => {
       .text();
     newsTitles.push(newsTitle);
   });
+  // console.log(newsTitles);
   return {
     props: { newsTitles }, // will be passed to the page component as props
   };
 };
 
-export default function News({ newsTitles }) {
+export default function News({ newsTitles }: {newsTitles:any}) {
   // console.log(newsTitles);
   return (
     <div>
@@ -49,8 +51,9 @@ export default function News({ newsTitles }) {
       <Navbar />
       <main className="bg-blue-200">
         <div className="flex flex-wrap flex-col items-center gap-5">
-            {newsTitles.map((item, key) => (
-                <NewsCard
+            {newsTitles.map((item:any, key:any) => (
+              <NewsCard
+                  key={key}
                   title={item.h2}
                   subtitle={item.h3}
                   author={item.author}
